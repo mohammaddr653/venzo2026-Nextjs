227b4e2a9aa321f567df8e221f9714b4645cec17
Ozone <mohammaddr653@gmail.com>
2025-11-03 13:47:15 +0330

    v17-added user and update-user page

---
+++ front-end/src/app/(admin)/admin/update-user/[userId]/page.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import axios from "axios";
+import { SERVER_API } from "../../../../../../config";
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import { useUserStore } from "@/store";
+import callManager from "@/hooks/callManager";
+
+const baseURL =
+  process.env.NEXT_PUBLIC_BASE_URL || "https://aminderakhshande.ir";
+
+export default function UpdateUserPage() {
+  const { call } = callManager();
+  const { user } = useUserStore();
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+  });
+  const { userId } = useParams<{ userId: string }>();
+
+  async function loadOneUser() {
+    const response = await call(
+      axios.get(SERVER_API + `/admin/dashboard/users/${userId}`),
+      false
+    );
+    const matchedUser = response.data.data;
+    setFormData({
+      ...formData,
+      name: matchedUser.name,
+      email: matchedUser.email,
+    });
+  }
+  useEffect(() => {
+    loadOneUser();
+  }, []);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const response = await call(
+      axios.put(SERVER_API + `/admin/dashboard/users/${userId}`, formData),
+      true
+    );
+  };
+
+  return (
+    <div>
+      <h1>مشاهده یک کاربر</h1>
+      <div className="my-form my-3">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-white flex flex-col gap-3 justify-center align-middle shadow-md p-3 rounded"
+        >
+          <input
+            type="text"
+            className="border rounded p-3"
+            placeholder="name"
+            name="name"
+            value={formData?.name}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            className="border rounded p-3"
+            placeholder="email"
+            name="email"
+            value={formData?.email}
+            onChange={handleChange}
+          />
+          <button>بروزرسانی</button>
+        </form>
+      </div>
+      <div className="bg-sky-600">this is tailwind</div>
+      <div className="bg-sky-300">
+        this is zustand , hello{user ? user.name : " user"}
+      </div>
+    </div>
+  );
+}
--- front-end/src/app/(admin)/admin/users/page.tsx
+++ front-end/src/app/(admin)/admin/users/page.tsx
@@ -1,8 +1,152 @@
 "use client";
 
+import axios from "axios";
+import { SERVER_API } from "../../../../../config";
+import { useEffect, useState } from "react";
+import { useUserStore } from "@/store";
+import callManager from "@/hooks/callManager";
+import { useRouter } from "next/navigation";
+
 const baseURL =
   process.env.NEXT_PUBLIC_BASE_URL || "https://aminderakhshande.ir";
 
 export default function UsersPage() {
-  return <div>users page</div>;
+  const { call } = callManager();
+  const { user } = useUserStore();
+  const [users, setUsers] = useState<any[]>([]);
+  const router = useRouter();
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    token: "",
+  });
+
+  async function loadUsers() {
+    const response = await call(
+      axios.get(SERVER_API + "/admin/dashboard/users"),
+      false
+    );
+    setUsers([...response.data.data]);
+  }
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleDelete = async (
+    e: React.MouseEvent<HTMLButtonElement>,
+    userId: any
+  ) => {
+    const response = await call(
+      axios.delete(SERVER_API + `/admin/dashboard/users/${userId}`),
+      true
+    );
+    loadUsers();
+  };
+
+  const handleUpdate = async (
+    e: React.MouseEvent<HTMLButtonElement>,
+    userId: any
+  ) => {
+    router.push(`/admin/update-user/${userId}`);
+  };
+
+  const handleRefresh = () => {
+    loadUsers();
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const response = await call(
+      axios.post(SERVER_API + "/admin/dashboard/users", formData),
+      true
+    );
+  };
+
+  return (
+    <div>
+      <h1>مدیریت کاربران</h1>
+      <div className="bg-red-300">
+        <h1>ثبت نام</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            placeholder="name"
+            className="border"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="email"
+            className="border"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="password"
+            placeholder="password"
+            className="border"
+            onChange={handleChange}
+          />
+          <button>ثبت نام</button>
+        </form>
+      </div>
+      <div className="bg-blue-300">
+        <button onClick={handleRefresh}>refresh</button>
+        <table className="border">
+          <caption>list of users</caption>
+          <thead>
+            <tr>
+              <th className="border">id</th>
+              <th className="border">name</th>
+              <th className="border">email</th>
+              <th className="border">password</th>
+              <th className="border">operation</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users?.map((user: any, index: any) => {
+              return (
+                <tr key={index}>
+                  <td className="border">{user._id}</td>
+                  <td className="border">{user.name}</td>
+                  <td className="border">{user.email}</td>
+                  <td className="border">{user.password}</td>
+                  <td className="border">
+                    <button
+                      onClick={(e, userId = user._id) => {
+                        handleDelete(e, userId);
+                      }}
+                    >
+                      REMOVE
+                    </button>
+                    <button
+                      onClick={(e, userId = user._id) => {
+                        handleUpdate(e, userId);
+                      }}
+                    >
+                      UPDATE
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+
+      <div className="bg-sky-600">this is tailwind</div>
+      <div className="bg-sky-300">
+        this is zustand , hello{user ? user.name : " user"}
+      </div>
+    </div>
+  );
 }
