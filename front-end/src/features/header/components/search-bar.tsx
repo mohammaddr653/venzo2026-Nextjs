ccdf8e460bcfe7e56be3af679e72f402b1aff789
Ozone <mohammaddr653@gmail.com>
2025-11-03 20:14:52 +0330

    v22-added search icon to search bar

--- front-end/src/features/header/components/search-bar.tsx
+++ front-end/src/features/header/components/search-bar.tsx
@@ -1,10 +1,17 @@
-const SearchBar = (props:any) => {
+import SearchSvg from "@/components/icons/search-svg";
+
+const SearchBar = (props: any) => {
   return (
-    <input
-      type="text"
-      className={`focus:bg-white focus:border-primary focus:outline-0 border rounded-full border-cu-neutral-700 font-weight300 py-2 w-full px-3 text-size14 ${props.className}`}
-      placeholder="جستجو..."
-    />
+    <div className="rounded-full overflow-hidden focus:outline-0 border border-primary/70 px-2 flex justify-start items-stretch">
+      <span className="flex text-neutral-primary">
+        <SearchSvg width={30} stroke={"currentColor"}></SearchSvg>
+      </span>
+      <input
+        type="text"
+        className={` font-weight300 py-2 w-full px-1 text-size14 ${props.className} text-primary`}
+        placeholder="جستجو..."
+      />
+    </div>
   );
 };
 export default SearchBar;
