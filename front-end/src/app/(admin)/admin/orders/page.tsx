0359231f4d005e6b3321b0738282f19500f1d904
Ozone <mohammaddr653@gmail.com>
2025-12-31 01:38:49 +0330

    v141-orders CRUD UI created

--- front-end/src/app/(admin)/admin/orders/page.tsx
+++ front-end/src/app/(admin)/admin/orders/page.tsx
@@ -6,13 +6,12 @@ import { SERVER_API } from "../../../../../config";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import callManager from "@/hooks/callManager";
-import { useUserStore } from "@/store";
+import TitleRight from "@/components/title-right";
 
 const baseURL =
   process.env.NEXT_PUBLIC_BASE_URL || "https://aminderakhshande.ir";
 
 export default function OrdersPage() {
-  const { user } = useUserStore();
   const [orders, setOrders] = useState<any>([]);
   const { call } = callManager();
   const [selectedOrder, setSelectedOrder] = useState<any>(null);
@@ -48,36 +47,41 @@ export default function OrdersPage() {
 
   return (
     <>
-      <main className="pt-15">
-        <div className="client-orders-page-container flex flex-col gap-5">
-          <h1>all orders page</h1>
-          <div>
-            <table>
-              <caption>لیست سفارش های شما</caption>
-              <thead>
-                <tr>
-                  <th>شناسه</th>
-                  <th>قیمت کل</th>
-                  <th>وضعیت</th>
-                  <th>عملیات</th>
-                </tr>
-              </thead>
-              <tbody>
-                {orders?.map((order: any, index: any) => {
-                  return (
-                    <tr key={index} className="border">
-                      <td>{order._id}</td>
-                      <td>{order.totalPrice} تومان</td>
-                      <td>
-                        <OrderStatus order={order}></OrderStatus>
-                        {order.authority !== "" && (
-                          <p>شناسه پرداخت : {order.authority}</p>
-                        )}
-                        {order.referenceId !== "" && (
-                          <p>کد رهگیری : {order.referenceId}</p>
-                        )}
-                      </td>
-                      <td className="flex flex-row gap-2">
+      <div className="client-orders-page-container flex flex-col gap-5">
+        <TitleRight
+          title="مدیریت سفارش ها"
+          className={"text-wrap"}
+        ></TitleRight>
+        <div className="bg-neutral-100 flex flex-col gap-5 p-2 grow">
+          <div className="flex justify-between items-center">
+            <p>لیست سفارشات</p>
+          </div>
+          <table className="cu-records-table w-full text-sm">
+            <thead>
+              <tr>
+                <th>شناسه</th>
+                <th>قیمت کل</th>
+                <th>وضعیت</th>
+                <th>عملیات</th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders?.map((order: any, index: any) => {
+                return (
+                  <tr key={index}>
+                    <td>{order._id}</td>
+                    <td>{order.totalPrice} تومان</td>
+                    <td>
+                      <OrderStatus order={order}></OrderStatus>
+                      {order.authority !== "" && (
+                        <p>شناسه پرداخت : {order.authority}</p>
+                      )}
+                      {order.referenceId !== "" && (
+                        <p>کد رهگیری : {order.referenceId}</p>
+                      )}
+                    </td>
+                    <td>
+                      <div className="flex flex-row gap-2">
                         {order.authority !== "" && (
                           <>
                             <button
@@ -87,34 +91,34 @@ export default function OrdersPage() {
                               جزئیات سفارش
                             </button>
                             <button
-                              className="p-2 bg-red-500 border"
+                              className="p-2 bg-green-500 border rounded-md"
                               onClick={(e) => handleVerify(e, order.authority)}
                             >
                               اعتبار سنجی
                             </button>
                             <button
-                              className="p-2 bg-red-500 border"
+                              className="p-2 bg-red-500 border rounded-md"
                               onClick={(e) => handleInquiry(e, order.authority)}
                             >
                               استعلام
                             </button>
                           </>
                         )}
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
-        {selectedOrder && (
-          <ExpandedOrder
-            selectedOrder={selectedOrder}
-            setSelectedOrder={setSelectedOrder}
-          ></ExpandedOrder>
-        )}
-      </main>
+      </div>
+      {selectedOrder && (
+        <ExpandedOrder
+          selectedOrder={selectedOrder}
+          setSelectedOrder={setSelectedOrder}
+        ></ExpandedOrder>
+      )}
     </>
   );
 }
